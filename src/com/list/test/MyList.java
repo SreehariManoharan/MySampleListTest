88cf9e41162065416721afb288abdedad96da8e8
SreehariManoharan <sreeharimanoharan42@gmail.com>
2023-06-16 06:35:08 +0200

    containse funcion added for the custom list

--- src/com/list/test/MyList.java
+++ src/com/list/test/MyList.java
@@ -1,32 +1,44 @@
 package com.list.test;
 
+import java.util.Arrays;
 
-public class MyList{
-	
+public class MyList {
+
 	private static final int INITIAL_SIZE = 10;
-    private Object[] elements;
-    private int size;
-    
-    public MyList() {
-        elements = new Object[INITIAL_SIZE];
-        size = 0;
-    }
-	  public void add(Object element) {
-	        elements[size++] = element;
-	    }
-	  public Object get(int currentIndex) {
-		  return elements[currentIndex];
-	  }
-	  
-	  public int size() {
-		  return size;
-	  }
+	private Object[] elements;
+	private int size;
+
+	public MyList() {
+		elements = new Object[INITIAL_SIZE];
+		size = 0;
+	}
+
+	public void add(Object element) {
+		elements[size++] = element;
+	}
+
+	public Object get(int currentIndex) {
+		return elements[currentIndex];
+	}
+
+	public int size() {
+		return size;
+	}
+
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
 		return size == 0;
 	}
+
 	public void remove(int i) {
 		System.arraycopy(elements, i + 1, elements, i, 1);
 		elements[--size] = null;
+	}
+
+	public Object contains(Object searchElement) {
+		return Arrays.asList(elements)
+		.stream()
+		.filter(eachElement -> eachElement.equals(searchElement))
+		.findAny()
+		.isPresent();
 	}
 }
