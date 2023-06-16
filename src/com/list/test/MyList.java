0bb34cb0b8a0747e638ff60202190085066c1cc5
SreehariManoharan <sreeharimanoharan42@gmail.com>
2023-06-16 05:53:31 +0200

    Test case passed for the size functionality

--- src/com/list/test/MyList.java
+++ src/com/list/test/MyList.java
@@ -5,16 +5,20 @@ public class MyList{
 	
 	private static final int INITIAL_SIZE = 10;
     private Object[] elements;
-    private int index;
+    private int size;
     
     public MyList() {
         elements = new Object[INITIAL_SIZE];
-        index = 0;
+        size = 0;
     }
 	  public void add(Object element) {
-	        elements[index++] = element;
+	        elements[size++] = element;
 	    }
 	  public Object get(int currentIndex) {
 		  return elements[currentIndex];
+	  }
+	  
+	  public int size() {
+		  return size;
 	  }
 }
